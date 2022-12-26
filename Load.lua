456c24cfdddd5b278c0427d22588d2464b0d1b4c
OwnAll <121460182+OwnAll@users.noreply.github.com>
2022-12-26 21:46:35 +0100

    Create Load.lua

---
+++ Load.lua
@@ -0,0 +1,9 @@
+local PlaceId = game.PlaceId
+
+if PlaceId == 537413528 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/OwnAll/OwnAllHub/main/Main.lua',true))()
+    else
+ game.Players.LocalPlayer:kick("Game Not Support.")
+ wait(1)
+ game:Shutdown()
+end
